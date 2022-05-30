74c9a4ff11243b010211be30688376f56532ed74
Clipporion <marcusberger008@gmx.net>
2022-05-30 20:00:25 +0200

    Add function that asks player for choice
    I added a function that asks the player for rock, paper or scissors
    and convert it toLowerCase
    If the choice is neither of the three, to prompt comes up again
    until a valid choice is made

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let player = "";
-let computer = "";
-
 function computerGuess() {
-    computer = Math.floor(Math.random()*3+1);
-    if (computer === 1) {
-        computer = "rock";
-    } else if (computer === 2) {
-        computer = "paper";
+    guess = Math.floor(Math.random()*3+1);
+    if (guess === 1) {
+        guess = "rock";
+    } else if (guess === 2) {
+        guess = "paper";
     } else {
-        computer = "scissors";
+        guess = "scissors";
     }
-    console.log(computer)
+    return guess;
+}
+
+function playerGuess() {
+        x = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+        if (x == "rock" || x == "paper" || x == "scissors") {
+            return x;
+        } else {
+            playerGuess();
+        }
 }
